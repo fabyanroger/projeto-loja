3ff3ad9500894bd3f4af685289e0dab32ccda6e8
Fabyan Roger <fabyanroger@hotmail.com>
2024-02-22 02:23:24 -0300

    Primeiro commit com meu projeto Loja

---
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,55 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos") /* Como o Produto da class é diferente de produtos do banco de dados, utilizo @Table */
+public class Produto {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    /* @Column(name = "desc")  O mesmo para caso eu nomes diferentes da classe e o banco de dados */
+    private String descricao;
+    private BigDecimal preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
